4e057530419b403fad6c9a8967988fd20478f115
zxBCN Romero,Jorge (IT EDS) EXTERNAL <jorge.romero.ext@boehringer-ingelheim.com>
2021-02-15 18:57:32 +0100

    wait for components of type ods to be created.

--- src/test/groovy/org/ods/e2e/util/ProjectHelper.groovy
+++ src/test/groovy/org/ods/e2e/util/ProjectHelper.groovy
@@ -6,6 +6,9 @@ import org.ods.e2e.provapp.util.ProvAppClient
 
 class ProjectHelper {
 
+
+    public static final String METADATA_YAML_TEMPLATE = '/metadata.yaml.template'
+
     def provisionProject(def project) {
         if (!project) {
             throw new MissingArgumentException("It's needed to provide the project definition")
@@ -17,25 +20,35 @@ class ProjectHelper {
         // Create project
         def provAppClient = new ProvAppClient()
         def generatedProjectData = provAppClient.createRandomProject(project.projectDefinition)
-        println generatedProjectData
+        println("Generated project data: $generatedProjectData")
+
 
         def projectKey = generatedProjectData.projectKey.toLowerCase()
-        println("Project generated: $projectKey")
+        println("Generated project key: $projectKey")
 
         // Wait until the project jenkins instance is running
         def openshiftClient = OpenShiftClient.connect("$projectKey-cd")
         openshiftClient.waitForDeployment('jenkins', 0)
+        UriHelper.waitURLAvailable(generatedProjectData.platformBuildEngineUrl, 300, 10)
 
         // Add components
         openshiftClient = OpenShiftClient.connect("$projectKey-dev")
         project?.components.each { component ->
             def data = provAppClient.addComponentsToProject(
                     [projectKey   : projectKey,
-                     quickstarters: [component]
+                     quickstarters: [[component_type: component.qs, component_id: component.id]]
                     ]
             )
-            openshiftClient.waitForDeployment(component.component_id, 0)
+            if (component.type == 'ods') {
+                openshiftClient.waitForDeployment(component.id, 0)
+            }
         }
         return generatedProjectData
+    }
+
+    def generateMetadataYaml(bindings) {
+        def text = getClass().getResourceAsStream(METADATA_YAML_TEMPLATE).text
+        def engine = new groovy.text.GStringTemplateEngine()
+        def template = engine.createTemplate(text).make(bindings)
     }
 }
